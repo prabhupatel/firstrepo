f9b206c94a06c2ce41afb3b07b9dc853924dd476
prabhupatel <prabhu.contact@gmail.com>
2015-08-09 09:50:52 -0500

    first checkin

---
+++ PersonRestController.java
@@ -0,0 +1,25 @@
+package rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import service.PersonService;
+import domain.Person;
+
+@RestController
+public class PersonRestController {
+	
+	@Autowired
+	PersonService personService;
+	
+	
+	@RequestMapping("/api/persion")
+	public String getPerson(){
+		//return personService.findPersonByName(name);
+		return "prabhu";
+		
+	}
+
+}
